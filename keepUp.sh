afd206a7f47122d77d47744dc88110af4ed780da
hectorgoan <hectorgoan@usal.es>
2016-11-09 12:41:41 +0100

    upgrage -> upgrade

--- keepUp.sh
+++ keepUp.sh
@@ -1,2 +1,2 @@
 #Auto updater/upgrader/cleaner for debian-based linux distros
-sudo apt-get update && sudo apt-get upgrage && sudo apt-get dist-upgrade && sudo apt-get autoremove && sudo apt-get clean
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade && sudo apt-get autoremove && sudo apt-get clean
