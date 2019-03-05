c931529e09cf8c538747c570b3915f1f43b5a361
Héctor Go. An <hgonzalo@undanet.com>
2019-03-05 09:16:59 +0100

    Added 'upgrade_oh_my_zsh' command

--- keepUp.sh
+++ keepUp.sh
@@ -2,12 +2,12 @@
 #Auto updater/upgrader/cleaner for debian-based linux distros
 if [ $# -eq 0 ]
   then
-    sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade && sudo apt-get autoremove && sudo apt-get clean
+    sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade && sudo apt-get autoremove && sudo apt-get clean && upgrade_oh_my_zsh
 fi
 if [ $# -eq 1 ]
   then
     if [ $1 == "y" ]
       then
-    	sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove && sudo apt-get -y clean
+    	sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove && sudo apt-get -y clean && upgrade_oh_my_zsh
     fi
 fi
