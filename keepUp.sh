0d7c2e6d30c9ad10968b95b0838f7df6c4fdc90a
Héctor Go. An <hgonzalo@undanet.com>
2019-03-05 15:53:27 +0100

    Fixed the unability to update ohMyZsh from outside the 'keepUp.sh' containing directory

--- keepUp.sh
+++ keepUp.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #Auto updater/upgrader/cleaner for debian-based linux distros
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 if [ $# -eq 0 ]
   then
-    sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade && sudo apt-get autoremove && sudo apt-get clean && ./upgradeZSH.sh
+    sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade && sudo apt-get autoremove && sudo apt-get clean && source ${__dir}/upgradeZSH.sh
 fi
 if [ $# -eq 1 ]
   then
     if [ $1 == "y" ]
       then
-    	sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove && sudo apt-get -y clean && ./upgradeZSH.sh
+    	sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove && sudo apt-get -y clean && ${__dir}/upgradeZSH.sh
     fi
 fi
