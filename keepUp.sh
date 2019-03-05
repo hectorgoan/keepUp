06cc1d83a73df398be8d7225b790e9ea11885cf7
Héctor Go. An <hgonzalo@undanet.com>
2019-03-05 15:46:39 +0100

    Added zsh upgrading capabilities

--- keepUp.sh
+++ keepUp.sh
@@ -2,12 +2,12 @@
 #Auto updater/upgrader/cleaner for debian-based linux distros
 if [ $# -eq 0 ]
   then
-    sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade && sudo apt-get autoremove && sudo apt-get clean && upgrade_oh_my_zsh
+    sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade && sudo apt-get autoremove && sudo apt-get clean && ./upgradeZSH.sh
 fi
 if [ $# -eq 1 ]
   then
     if [ $1 == "y" ]
       then
-    	sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove && sudo apt-get -y clean && upgrade_oh_my_zsh
+    	sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove && sudo apt-get -y clean && ./upgradeZSH.sh
     fi
 fi
