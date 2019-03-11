677b4ce41db5c94ca3fdbe5888f6edc55935c8f7
Héctor Go. An <hgonzalo@undanet.com>
2019-03-11 13:12:19 +0100

    args shortened

--- keepUp.sh
+++ keepUp.sh
@@ -8,23 +8,23 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 if [ $# -eq 0 ]
   then
     echo "Please, specify your system's OS with the following options:";
-    echo "	linux	-> for debian-based linux distros with ohMyZsh installed";
-    echo "	mac 	-> for darwin systems with homebrew & ohMyZsh installed";
+    echo "	l	-> for debian-based linux distros with ohMyZsh installed";
+    echo "	m 	-> for darwin systems with homebrew & ohMyZsh installed";
 fi
 if [ $# -eq 1 ]
   then
-  	if [ $1 == "linux" ]
+  	if [ $1 == "l" ]
   		then
   		sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade && sudo apt-get autoremove && sudo apt-get clean && source ${__dir}/upgradeZSH.sh
   	fi
-  	if [ $1 == "mac" ]
+  	if [ $1 == "m" ]
   		then
   		brew doctor && brew update && brew upgrade && brew cleanup && source ${__dir}/upgradeZSH.sh
   	fi
 fi
 if [ $# -eq 2 ]
   then
-    if [[ $1 == "linux" ]] && [[ $2 == "y" ]]
+    if [[ $1 == "l" ]] && [[ $2 == "y" ]]
       	then
     	sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove && sudo apt-get -y clean && ${__dir}/upgradeZSH.sh
     fi
