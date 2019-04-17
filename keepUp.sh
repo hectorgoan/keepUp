be6e8fe3f6ac33257f10463faa68bdfc8ddfb111
Héctor Go. An <hectorgoan@usal.es>
2019-04-18 01:03:22 +0200

    Updated the script to work well when using the fish shell

--- keepUp.sh
+++ keepUp.sh
@@ -10,6 +10,10 @@ if [ $# -eq 0 ]
     echo "Please, specify your system's OS with the following options:";
     echo "	l	-> for debian-based linux distros with ohMyZsh installed";
     echo "	m 	-> for darwin systems with homebrew & ohMyZsh installed";
+    echo "	f l 	-> for debian-based linux distros withOUT ohMyZsh installed";
+    echo "	f m 	-> for darwin systems with homebrew & withOUT ohMyZsh installed";
+    echo "	NOTE: If you specify the y option when executing in a linux system,";
+    echo "	it'll automatically answer yes to all the questions. ";
 fi
 if [ $# -eq 1 ]
   then
@@ -26,6 +30,21 @@ if [ $# -eq 2 ]
   then
     if [[ $1 == "l" ]] && [[ $2 == "y" ]]
       	then
-    	sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove && sudo apt-get -y clean && ${__dir}/upgradeZSH.sh
+    	sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove && sudo apt-get -y clean && source ${__dir}/upgradeZSH.sh
+    fi
+    if [[ $1 == "f" ]] && [[ $2 == "l" ]]
+      	then
+  		sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade && sudo apt-get autoremove && sudo apt-get clean
+    fi
+    if [[ $1 == "f" ]] && [[ $2 == "m" ]]
+      	then
+  		brew doctor && brew update && brew upgrade && brew cleanup
+    fi
+fi
+if [ $# -eq 3 ]
+  then
+    if [[ $1 == "f" ]] && [ $2 == "l" ]] && [[ $3 == "y" ]]
+      	then
+    	sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove && sudo apt-get -y clean
     fi
 fi
